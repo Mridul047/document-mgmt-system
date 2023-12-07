1da54f0c259c763aeb38ed860eb74b5e9bcc1c69
Mridul Choudhary <mridul.choudhary7@gmail.com>
2023-12-07 13:09:48 +0530

    first commit

---
+++ src/test/java/com/mycodethesaurus/documentmgmtsystem/DocumentMgmtSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycodethesaurus.documentmgmtsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentMgmtSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
